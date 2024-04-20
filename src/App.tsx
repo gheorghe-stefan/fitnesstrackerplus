9004fdfc550ef4ed2a50419c06cd0593251cbb48
Stefan Gheorghe <stefangh@gmail.com>
2024-04-20 22:07:14 +0200

    WIP UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { AppBar, Box, Button, Dialog, Icon, IconButton, ThemeProvider, Toolbar, Tooltip, Typography, createTheme } from '@mui/material';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
+  const [isTreadmillConnected, setTreadmillConnection] = useState(false);
+  const [isHRConnected, setHRConnection] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="left-container">
+          <h1>FitnessTrackerPlus</h1>
+        </div>
+        <div className="right-container">
+          <Tooltip title="Connect a heart rate sensor">
+            <IconButton className={isHRConnected ? 'button' : 'button disconnected'} onClick={() => setHRConnection(!isHRConnected)}>
+              {isHRConnected ? 'Disconnect HR' : 'Connect HR'}
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Connect to a treadmill">
+            <IconButton className={isTreadmillConnected ? 'button' : 'button disconnected'} onClick={() => setTreadmillConnection(!isTreadmillConnected)}>
+              {isTreadmillConnected ? 'Disconnect Treadmill' : 'Connect Treadmill'}
+            </IconButton>
+          </Tooltip>
+        </div>
       </header>
+      <footer className="footer">
+        <IconButton>Settings</IconButton>
+      </footer>
     </div>
   );
 }
